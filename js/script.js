e7f1cdb6f8f896332211fa740554f154f5eaf013
hongyinggame <1412159107@qq.com>
2024-06-28 19:50:27 +0800

    Site updated: 2024-06-28 19:50:13

--- js/script.js
+++ js/script.js
@@ -135,3 +135,17 @@
     $container.removeClass('mobile-nav-on');
   });
 })(jQuery);
+<script src="https://cdn.jsdelivr.net/npm/bluebird@3/js/browser/bluebird.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/whatwg-fetch@2.0.3/fetch.min.js"></script>
+fetch('https://v1.hitokoto.cn')
+    .then(function (res){
+      return res.json();
+    })
+    .then(function (data) {
+      var hitokoto = document.getElementById('hitokoto');
+      hitokoto.innerText = data.hitokoto + '——【' + data.from + '】';
+    })
+    .catch(function (err) {
+      console.error(err);
+    });
+
