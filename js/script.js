794fa2a59dafaca1954eb92cd8940fcf1c779d44
‘git’ <1412159107@qq.com>
2025-11-25 13:08:14 +0800

    Site updated: 2025-11-25 13:08:12

--- js/script.js
+++ js/script.js
@@ -134,4 +134,17 @@
 
     $container.removeClass('mobile-nav-on');
   });
+  fetch('https://v1.hitokoto.cn')
+    .then(function (res){
+      return res.json();
+    })
+    .then(function (data) {
+      var hitokoto = document.getElementById("hitokoto");
+      hitokoto.innerText = data.hitokoto + '——【' + data.from + '】';
+    })
+    .catch(function (err) {
+      console.error(err);
+    });
+
 })(jQuery);
+
